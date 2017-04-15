84c1ac9a5c9ce20b4c9557d442d095bec3227ca3
Sean Oberoi <sean.o@mac.com>
2017-04-15 11:33:43 -0700

    Fix up directory structure and other stuff

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-![milehigh](https://cloud.githubusercontent.com/assets/16991015/21538389/85cd3e8a-cd51-11e6-81d6-0b80d6899116.png)
 # x³ ( xQube )
 
 **Boilerplate for 3D rendering and server-side physics in HTML5 and NodeJS**
 
-##Mechanics
+## Mechanics
 * Core Engine: [*three.js*](https://threejs.org/)
 * Networking Backend: [Socket.IO](http://socket.io/) || [Pure WebSockets](https://github.com/xseano/xQube/tree/master/server/src/lib/)
 
-##Releases
+## Releases
 * [Socket.IO Implementation v1.0.0](https://github.com/xseano/xQube/releases/tag/v1.0.0-Socket.IO)
 * WebSockets Implementation v1.0.0 - Coming Soon!
 
-##Preview
+## Preview
 ![](https://i.gyazo.com/4b299d5057378ee699ac0a77f4e3d4c5.gif)
---
+++ html/index.php
@@ -0,0 +1,24 @@
+<?php
+function crc32b($ptf) {
+  return hash_file('crc32b', $ptf);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+	  <meta charset="UTF-8">
+	  <title>x³ | xQube</title>
+	</head>
+
+  <body>
+		<div id="bodyDiv"></div>
+    <link href="./resources/css/xqube.css?v=<?php echo crc32b('./resources/css/xqube.css'); ?>" rel="stylesheet">
+		<script src="./resources/js/three.js?v=<?php echo crc32b('./resources/js/three.js'); ?>"></script>
+		<script src="./resources/js/socket.io.js?v=<?php echo crc32b('./resources/js/socket.io.js'); ?>"></script>
+		<script src="./resources/js/main.js?v=<?php echo crc32b('./resources/js/main.js'); ?>"></script>
+		<script src="./resources/js/jquery-1.12.4.min.js?v=<?php echo crc32b('./resources/js/jquery-1.12.4.min.js'); ?>"></script>
+	</body>
+
+</html>
