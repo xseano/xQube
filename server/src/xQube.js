2737fa8d6eb260df144e68769e64b9d858c51598
Sean Oberoi <sean.o@mac.com>
2017-04-30 08:26:41 -0700

    add useStrict function

--- server/src/xQube.js
+++ server/src/xQube.js
@@ -1,3 +1,13 @@
+useStrict(true);
+
+function useStrict(option) {
+  if (option == true) {
+    "use strict";
+  } else if (option == false) {
+    "don't use strict";
+  }
+}
+
 const WebSocket = require('ws');
 var obj = this;
 const Block = require('./xclass/Block');
